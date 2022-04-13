ed3a4e3b5fe7ff1f3e6b6af25fbddb5e1183a380
VladimirDanilov97 <dan1997ford@yandex.ru>
2022-04-13 16:36:01 +0300

    Create parser with aiohttp but it works as synchronic function

--- habr.py
+++ habr.py
@@ -1,21 +1,52 @@
-import requests
 from bs4 import BeautifulSoup
 from pprint import pprint
 import asyncio
 import aiohttp
+import logging
+import time
 
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(message)s')
+# logging.disable(logging.DEBUG)
 
-KEYWORDS = ['дизайн', 'фото', 'web', 'python']
+KEYWORDS = ['дизайн', 'фото', 'web', 'python', ]
 url = 'https://habr.com/ru/all/'
 
-response = requests.get(url=url)
-soup = BeautifulSoup(response.text, 'html.parser')
+async def scrap_content_page(link):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url=link) as resp:
+                    title_soup = BeautifulSoup(await resp.text(), 'html.parser')
+                    text = title_soup.find(id='post-content-body').text.lower()
+    return text
 
-titles = soup.find_all('a', class_='tm-article-snippet__title-link')
-articles = list(zip(
-    [time['datetime'] for time in soup.find_all('time')],
-    [title.text if set(KEYWORDS)&set(title.text.split())!=set() else None for title in titles ],
-    [title['href'] if set(KEYWORDS)&set(title.text.split())!=set() else None for title in titles]
-))
-articles = list(filter(lambda article: article[2] != None and article[1] != None , articles))
-pprint(articles)
+    
+async def get_data():
+    result = []
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url=url) as response:
+            response_text = await response.text()
+            soup = BeautifulSoup(response_text, 'html.parser')
+            times = soup.find_all('time')
+            titles = soup.find_all('a', class_='tm-article-snippet__title-link')
+            all_keywords = soup.find_all('div', class_='tm-article-snippet__hubs')
+            zipped = list(zip(titles, all_keywords, times))
+            for title, keyword, time in zipped:
+                logging.debug(f'{title.text}')
+                link = 'https://habr.com'+title['href']
+                text = await scrap_content_page(link)
+                logging.debug(f'2 = {title.text}')
+                if (
+                set(keyword) & set(KEYWORDS) != set()) or (
+                set(text.split()) & set(KEYWORDS) != set()) or (
+                set(title.text.split()) & set(KEYWORDS) != set()):
+                    result.append([
+                        time['datetime'],
+                        title.text,
+                        link])
+    logging.info(f'{result}')
+
+async def main():
+    await get_data()
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
